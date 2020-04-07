e72e2faad9ec6098ce7ea7f458002df6673e59d3
Armando <rivera.armando997@gmail.com>
2020-04-07 14:49:16 -0500

    Zero and one parents problem solution

--- src/index.js
+++ src/index.js
@@ -3,8 +3,45 @@
  * @param {number[][]} parentChildPairs
  * @returns {number[][]}
  */
-const findNodesWithZeroAndOneParents = (parentChildPairs) => {
 
+const findNodesWithZeroAndOneParents = (parentChildPairs) => {
+	// Parents
+	let parents=[];
+	parentChildPairs.map((Parents) => {
+		if (!(parents.includes(Parents[0])))
+
+			parents.push(Parents[0]);
+	});
+
+	// Childrens
+	let childrends=[];
+	let childrendsMoreThanOneParent =[];
+	parentChildPairs.map((Childrends) => {
+		if (childrends.includes(Childrends[1]))
+			childrendsMoreThanOneParent.push(Childrends[1]);
+		else
+			childrends.push(Childrends[1]);
+	});
+
+	// Zero parents
+	let zeroParents = [];
+	parents.map((Parents) => {
+		if (!(childrends.includes(Parents)))
+			zeroParents.push(Parents);
+	});	
+	
+	let exactlyOneParent = [];
+	childrends.map((Childrends) => {
+		if (!(childrendsMoreThanOneParent.includes(Childrends)))
+			exactlyOneParent.push(Childrends);
+	});
+
+	// Ordenamos los arrays
+	zeroParents.sort((a,b)=>a-b);
+	exactlyOneParent.sort((a,b)=>a-b);
+
+	return [zeroParents,exactlyOneParent];
+	
 }
 
 module.exports = findNodesWithZeroAndOneParents;

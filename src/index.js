ad86fdc99d383323ea14bceb72862dd7cbc6b1f6
Jonatan Lazo <jonatan.lazo@lertait.com>
2020-05-16 20:12:43 -0500

    Se implementa la funcio indicada

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@
  * @returns {number[][]}
  */
 const findNodesWithZeroAndOneParents = (parentChildPairs) => {
-
-}
+  let totalParentsMap = new Map();
+  parentChildPairs.forEach((element) => {
+    if (!totalParentsMap.has(element[0])) {
+      totalParentsMap.set(element[0], 0);
+    }
+    let totalParent = totalParentsMap.get(element[1]) || 0;
+    totalParentsMap.set(element[1], ++totalParent);
+  });
+  let zeroParents = [];
+  let oneParent = [];
+  for (let [key, value] of totalParentsMap) {
+    if (value === 0) {
+      zeroParents.push(key);
+      continue;
+    }
+    if (value === 1) oneParent.push(key);
+  }
+  return [zeroParents, oneParent];
+};
 
 module.exports = findNodesWithZeroAndOneParents;

85d48b5368a4f189d2a916a2db644973cf2614ae
Danelia Sanchez <danelia_sanchez20@hotmail.com>
2020-05-23 20:34:47 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@
  * @returns {number[][]}
  */
 const findNodesWithZeroAndOneParents = (parentChildPairs) => {
+  let parents = [];
+  let children = [];
 
-}
+  for (let i = 0; i < parentChildPairs.length; i++) {
+    if (!parents.includes(parentChildPairs[i][0])) {
+      parents.push(parentChildPairs[i][0]);
+    }
+    children.push(parentChildPairs[i][1]);
+  }
+
+  let numberChild = children.reduce((countChild, child) => {
+    countChild[child] = (countChild[child] || 0) + 1;
+    return countChild;
+  }, {});
+
+  parents = parents.filter((item) => !children.includes(item));
+  children = children
+    .filter((item) => numberChild[item] === 1)
+    .sort((a, b) => a - b);
+
+  return [parents, children];
+};
 
 module.exports = findNodesWithZeroAndOneParents;

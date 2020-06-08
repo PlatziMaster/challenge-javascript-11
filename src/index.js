3c14c811837f246fbfef6a11ec98e798ad6d4f3d
Alejandro Medina <5almero5@gmail.com>
2020-06-08 16:18:52 -0500

    add Logical of function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@
  * @returns {number[][]}
  */
 const findNodesWithZeroAndOneParents = (parentChildPairs) => {
-
+    let parents = new Set(parentChildPairs.map(array => array[0]))
+    let childs = parentChildPairs.map(array => array[1])
+    let grandPa = [];
+    let divorceChilds = new Set();
+    parents.forEach(parent => {
+        if(childs.indexOf(parent) === -1){
+            grandPa.push(parent)
+        }
+    })
+    childs.forEach((child, index) => {
+        if(childs.indexOf(child) === index) {
+            divorceChilds.add(child)
+        }else if (divorceChilds.has(child)) {
+            divorceChilds.delete(child)
+        }
+    })
+    return [grandPa, [... divorceChilds].sort((a, b) => a - b)]
 }
 
 module.exports = findNodesWithZeroAndOneParents;

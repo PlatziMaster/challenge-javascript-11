86034eee29c8523de25a1f7d6f4b5a62c6dbe6ac
SanArGuz <santiago.arcilag@gmail.com>
2020-07-11 19:18:44 -0500

    Reto cumplido

--- src/index.js
+++ src/index.js
@@ -5,6 +5,36 @@
  */
 const findNodesWithZeroAndOneParents = (parentChildPairs) => {
 
+    let zeroParents = []
+    let oneParent = []
+    let flag = 0, flag1=0
+
+    //Zero parents?
+    for(i=0; i<parentChildPairs.length; i++){
+        for(j=0; j<parentChildPairs.length; j++){
+            if(parentChildPairs[i][0] == parentChildPairs[j][1]){
+                flag++
+            }
+            if(parentChildPairs[i][1] == parentChildPairs[j][1]){
+                flag1++
+            }
+        }
+        if (flag == 0){
+            zeroParents.push(parentChildPairs[i][0])
+        } else if (flag == 1) {
+            oneParent.push(parentChildPairs[i][0])
+        }
+        if (flag1 == 1){
+            oneParent.push(parentChildPairs[i][1])
+        }
+        flag1 = 0
+        flag = 0
+    }
+
+
+    zeroParents = new Set(zeroParents)
+    oneParent = new Set(oneParent)
+    return [[...zeroParents], [...oneParent]]
 }
 
 module.exports = findNodesWithZeroAndOneParents;

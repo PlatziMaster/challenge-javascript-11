e5a8c16ca42cc5dda37787fd09876306705f7b7a
oscar <oscar.pay4@gmail.com>
2020-07-22 21:24:12 -0500

    implement findNodesWithZeroAndOneParents function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,39 @@
  * @returns {number[][]}
  */
 const findNodesWithZeroAndOneParents = (parentChildPairs) => {
+    const zeroParents = []
+    const oneParent = []
+    const hashParents = {}
 
+    for (let parentChildPair of parentChildPairs) {
+        const parent = parentChildPair[0];
+        const son = parentChildPair[1];
+
+        if (hashParents[son]) {
+            hashParents[son]++
+        } else {
+            hashParents[son] = 1
+        }
+
+        if (!hashParents[parent]) {
+            hashParents[parent] = 0
+        }
+
+    }
+
+    for (let num in hashParents) {
+        if(hashParents[num] === 0) {
+            zeroParents.push(Number(num))
+            continue
+        }
+
+        if(hashParents[num] === 1) {
+            oneParent.push(Number(num))
+            continue
+        }
+    }
+
+    return [zeroParents, oneParent]
 }
 
 module.exports = findNodesWithZeroAndOneParents;

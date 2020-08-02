7da4f393b5e64a3ced878814fefd52bc3f7c7b48
María Andrea <pericoandrea@gmail.com>
2020-08-02 11:37:46 -0500

    ZeroAndOneParents Algorithm

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@
  * @returns {number[][]}
  */
 const findNodesWithZeroAndOneParents = (parentChildPairs) => {
-
+    const flatItems = parentChildPairs.flat();
+    const uniqueItems = flatItems.filter((item, index) => flatItems.indexOf(item) == index)
+    const parentsCount = new Array(uniqueItems.length).fill(0);
+    parentChildPairs.map(pairs =>{
+        parentsCount[uniqueItems.indexOf(pairs[1])]++;
+    });
+    const result = [[],[]];
+    parentsCount.map((count, index) => {
+        if(count < 2) result[count].push(uniqueItems[index])
+    });
+    return result;
 }
 
 module.exports = findNodesWithZeroAndOneParents;

5dedc6d23bbff4e662616fa69ed1722f8d9f2fd3
Xerox <alfredo.montoya@cesunbc.edu.mx>
2020-08-18 01:49:16 -0700

    Challenge #11 - Solved

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@
  * @returns {number[][]}
  */
 const findNodesWithZeroAndOneParents = (parentChildPairs) => {
+    let zeroParents = parentChildPairs.flat(); // El método de flat crea una nueva matriz con los elementos de la sub-matriz concatenados en ella.
+    //El método filter() crea un nuevo array con todos los elementos que cumplan la condición
+    zeroParents = zeroParents.filter((item, index) => zeroParents.indexOf(item) == index); // Contador de resultados
+    const parentCounter = new Array(zeroParents.length).fill(0); // Generamos el array espejo para el contador
+    parentChildPairs.map(pairs =>{ // Genera nuevo array con map de elementos cuyo índice sea 1
+        parentCounter[zeroParents.indexOf(pairs[1])]++; 
+    });
+    const result = [[],[]]; // Matríz que se llanará con el maping de los resultados
+    parentCounter.map((count, index) => {
+        if(count < 2) result[count].push(zeroParents[index]) // Concatena los elementos a zeroParents
+    });
+    return result;
 
 }
 
